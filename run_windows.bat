1e9a155c4177e3da79ab81cf03594a0fcbcd0bf2
scue <linkscue@gmail.com>
2014-05-07 14:18:37 +0800

    run_windows.bat: 在Windows中自动执行

---
+++ run_windows.bat
@@ -0,0 +1,16 @@
+echo off
+echo "=== spliting .."
+adb wait-for-device && adb reboot recovery
+adb wait-for-device && adb push busybox /tmp/busybox
+adb wait-for-device && adb push split_part.sh /tmp/split_part.sh
+adb wait-for-device && adb shell chmod 755 /tmp/busybox
+adb wait-for-device && adb shell chmod 755 /tmp/split_part.sh
+adb wait-for-device && adb shell /tmp/split_part.sh
+echo "=== formating .."
+adb wait-for-device && adb reboot recovery
+adb wait-for-device && adb push busybox /tmp/busybox
+adb wait-for-device && adb push format_part.sh /tmp/format_part.sh
+adb wait-for-device && adb shell chmod 755 /tmp/busybox
+adb wait-for-device && adb shell chmod 755 /tmp/format_part.sh
+adb wait-for-device && adb shell /tmp/format_part.sh
+@pause
