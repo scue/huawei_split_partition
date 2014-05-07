439d4639a8803dbce84566a4ef4c728c64981c2b
scue <linkscue@gmail.com>
2014-05-07 13:29:09 +0800

    split_part.sh: 重新分区

--- split_part.sh
+++ split_part.sh
@@ -53,13 +53,3 @@ $p19_start
 w
 
 EOF
-
-# 格式化
-#-------------------------------------------------------------------------------
-#  可能需要重启 recovery 后才可以正确格式化，出现如下错误：
-#      mke2fs: lseek: Value too large for defined data type
-#  表示 fdisk 分区还未生效，格式化 EXT4 时出现了错误，此时应当重启到 recovery
-#  重新执行再一次执行此脚本
-#-------------------------------------------------------------------------------
-$busybox mke2fs -T ext4 -L data /dev/block/mmcblk0p18
-$busybox mkdosfs -v -n huawei /dev/block/mmcblk0p19
