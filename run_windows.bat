170d698f04947bd9744afb3d15f7d003b376c64a
scue <linkscue@gmail.com>
2014-05-08 13:18:16 +0800

    run_windows.bat: 更新中文提示

--- run_windows.bat
+++ run_windows.bat
@@ -1,15 +1,17 @@
 echo off
-echo "=== spliting .."
-adb wait-for-device && adb reboot recovery
-adb wait-for-device && adb push busybox /busybox
-adb wait-for-device && adb push split_part.sh /split_part.sh
-adb wait-for-device && adb push e2fsck_s /e2fsck_s
-adb wait-for-device && adb push resize2fs_s /resize2fs_s
-adb wait-for-device && adb shell chmod 755 /busybox
-adb wait-for-device && adb shell chmod 755 /split_part.sh
-adb wait-for-device && adb shell chmod 755 /e2fsck_s
-adb wait-for-device && adb shell chmod 755 /resize2fs_s
-adb wait-for-device && adb shell /split_part.sh
-echo "=== all done"
-adb wait-for-device && adb reboot
+echo "=== 正在执行分区 .."
+.\adb reboot recovery
+echo "=== 请等待手机重启至Recovery，然后按下任意键"
+@pause
+.\adb push busybox /busybox
+.\adb push split_part.sh /split_part.sh
+.\adb push e2fsck_s /e2fsck_s
+.\adb push resize2fs_s /resize2fs_s
+.\adb shell chmod 755 /busybox
+.\adb shell chmod 755 /split_part.sh
+.\adb shell chmod 755 /e2fsck_s
+.\adb shell chmod 755 /resize2fs_s
+.\adb shell /split_part.sh
+echo "=== 分区操作完成"
+.\adb reboot
 @pause
