f9e5b4f3d2523e3a61e1d006e3152dca6041df25
scue <linkscue@gmail.com>
2014-05-08 13:22:14 +0800

    run_linux.sh: 更新 adb 的位置

--- run_linux.sh
+++ run_linux.sh
@@ -19,24 +19,24 @@
 
 split(){
     echo "==> spliting .."
-    adb wait-for-device && adb reboot recovery
-    adb wait-for-device && adb push busybox /busybox
-    adb wait-for-device && adb push e2fsck_s /e2fsck_s
-    adb wait-for-device && adb push resize2fs_s /resize2fs_s
-    adb wait-for-device && adb push split_part.sh /split_part.sh
-    adb wait-for-device && adb shell chmod 755 /busybox
-    adb wait-for-device && adb shell chmod 755 /split_part.sh
-    adb wait-for-device && adb shell chmod 755 /e2fsck_s
-    adb wait-for-device && adb shell chmod 755 /resize2fs_s
-    adb wait-for-device && adb shell /split_part.sh
+    ./adb wait-for-device && ./adb reboot recovery
+    ./adb wait-for-device && ./adb push busybox /busybox
+    ./adb wait-for-device && ./adb push e2fsck_s /e2fsck_s
+    ./adb wait-for-device && ./adb push resize2fs_s /resize2fs_s
+    ./adb wait-for-device && ./adb push split_part.sh /split_part.sh
+    ./adb wait-for-device && ./adb shell chmod 755 /busybox
+    ./adb wait-for-device && ./adb shell chmod 755 /split_part.sh
+    ./adb wait-for-device && ./adb shell chmod 755 /e2fsck_s
+    ./adb wait-for-device && ./adb shell chmod 755 /resize2fs_s
+    ./adb wait-for-device && ./adb shell /split_part.sh
 }
 
-device=$(adb devices | grep device$ ||\
-    adb devices | grep recovery$)
+device=$(./adb devices | grep device$ ||\
+    ./adb devices | grep recovery$)
 
 if [[ -z $device ]]; then
     echo "==> 没找到 Android 设备"
 else
     split
-    adb reboot
+    ./adb reboot
 fi
